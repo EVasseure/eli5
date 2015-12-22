198b6c40e7117f6d26a66ebc0dcd09dcab0008e4
Erwan VASSEURE <erwan.vasseure@canal-plus.com>
2015-12-22 10:40:36 +0100

    getting responses

--- eli5.py
+++ eli5.py
@@ -6,17 +6,31 @@ url = "http://www.reddit.com/r/explainlikeimfive/search?q={query}&restrict_sr=on
 
 
 @click.command()
-@click.option('--count', default=1, help='Number of answers.')
+@click.option('--count', default=5, help='Number questions proposed.')
 @click.option('--query', prompt='Search',
               help='Your search.')
 def hello(count, query):
     """Simple program that greets NAME for a total of COUNT times."""
-    print(query)
     response = urllib2.urlopen(url.format(query=query))
     soup = BeautifulSoup(response, 'html.parser')
     results = soup.findAll("div", {"class": "search-result"})
-    for r in results:
-        print(r.div.header.a.string.replace("ELI5:", ""))
+    for i, r in enumerate(results):
+        if i == count:
+            break
+        print("{nb} - {question}".format(nb=i, question=r.div.header.a.string))
+    while True:
+        try:
+            q_nb = int(raw_input("For which question would you like an awswer ?\n"))
+            if q_nb < count:
+                break
+            print("invalid value")
+        except ValueError:
+            print("invalid value")
+    response = urllib2.urlopen(results[q_nb].div.header.a['href'])
+    print(results[q_nb].div.header.a['href'])
+    # soup = BeautifulSoup(response, 'html.parser')
+    # response = soup.findAll("div", {"class": "usertext-body"})
+    # print(response[0])
 
 if __name__ == '__main__':
     hello()
